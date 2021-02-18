21eb61bc76b8b8f4d704049487f5a1b7bba72081
ALDO <aldo@aldos-mbp.attlocal.net>
2021-02-17 21:07:00 -0800

    3rd

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,9 @@
 import React from "react";
 import userRequest from "../hook/userRequest";
 import PhotoCard from "./PhotoCard";
-// import NavBar from "./Nav";
+import NavBar from "./Nav";
 import styled from "styled-components";
+import axios from "axios";
 
 export default function Data() {
   const { data, loading, error } = userRequest(
@@ -24,7 +25,16 @@ export default function Data() {
 
   return (
     <PhotoDiv>
-      <PhotoCard />
+      <NavBar />
+      <PhotoCard
+        // key={data.url}
+        // id={data.url}
+        url={data.url}
+        title={data.title}
+        info={data.explanation}
+        // // photo={data.url}
+        // aldo={data.serice_version}
+      />
     </PhotoDiv>
   );
 }
@@ -42,3 +52,9 @@ const LoadingDiv = styled.div`
   color: white;
   margin-top: 15%;
 `;
+
+console.log(
+  axios.get(
+    "https://api.nasa.gov/planetary/apod?api_key=PGZGMrCnZwWQRecXqsROalxcmvyhygGql0iLL1Of"
+  )
+);
